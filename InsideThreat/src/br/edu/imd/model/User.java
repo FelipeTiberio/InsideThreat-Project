7719e381fe099ae550b3e7bb465d0f4b8f9e393b
Felipe Tiberio <felipetiberio@yahoo.com.br>
2018-11-10 22:43:57 -0300

    Fiz uma primeira versão da classe user
    Prcisava de uma classe para testar os nós da árvore, então implementei user para usar com debug

---
+++ InsideThreat/src/br/edu/imd/model/User.java
@@ -0,0 +1,58 @@
+package br.edu.imd.model;
+
+
+
+public class User {
+	private String employer_name;
+	private String user_id;
+	private String email;
+	private String domain;
+	private String role;
+	
+	
+	public User() {
+		
+	}
+	
+	
+	public User(String employer_name, String user_id, String email, String domain, String role) {
+		super();
+		this.employer_name = employer_name;
+		this.user_id = user_id;
+		this.email = email;
+		this.domain = domain;
+		this.role = role;
+	}
+	
+	public String getEmployer_name() {
+		return employer_name;
+	}
+	public void setEmployer_name(String employer_name) {
+		this.employer_name = employer_name;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
