3bc466b18b69efabf9a6f207fa551dad022b0a3e
Felipe Tiberio <felipetiberio@yahoo.com.br>
2018-11-10 22:14:32 -0300

    Trando o fechamento do Arquivo

--- InsideThreat/src/br/edu/imd/model/LogAnalyzer.java
+++ InsideThreat/src/br/edu/imd/model/LogAnalyzer.java
@@ -1,31 +1,22 @@
-package csvReader;
-/**
- * 
- */
+package br.edu.imd.model;
+
+
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+//import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+
 
 public class LogAnalyzer {
-// A forma  que os dados estão organizados nos arquivos
-	private String format;
-	
-// Usado para armazenar as linhas dos arquivos	
-	private List<String[]> entries = new ArrayList<>(); 
-// Usado para percorrer o mapa, @TODO talvez não o use	
-	//private Iterator<String> stringIter;
-	
-	
-	
+	// Usado para armazenar as linhas dos arquivos	
+	private List<String[]> entries = new ArrayList<>();
+	private BufferedReader filebr = null; 
+
 	/**
 	 * O método é usado para ler uma arquivo scv linha por linha, cada linha é um índice do conteiner entries
 	 * @param filename Nome do arquivo a ser lido 
@@ -35,13 +26,11 @@ public class LogAnalyzer {
 	public void  readFile(String filename, String separate) throws IOException {
 		
 		try {
-			//FileInputStream file = new FileInputStream(filename);
-			//InputStreamReader input = new InputStreamReader(file);
-			//BufferedReader filebr = new BufferedReader(input);
-			BufferedReader filebr = new BufferedReader(new FileReader(filename));
+		
+			filebr  = new BufferedReader(new FileReader(filename));
 			String line;
 			
-			while((line = filebr.readLine()) != null) {
+			while((line = filebr .readLine()) != null) {
 				String filds[] = line.split(separate);
 				entries.add(filds);
 			}
@@ -53,9 +42,23 @@ public class LogAnalyzer {
 		}catch (IOException e) {
 			System.out.println(e.getCause());
 		}finally {
-			//@TODO lembra de fechar o arquivo 
+			if(filebr != null) {
+				try {
+					filebr.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+				
+			}
 		}
 		
+	}
+	
+	/**
+	 * @return Retorna uma lista de strings, cada string é uma linha do arquivo de log
+	 */
+	public List<String[]> getEntries() {
+		return entries;
 	}
 	
 	/**
