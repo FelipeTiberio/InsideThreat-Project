7150f8d340241d55304891bcecac154e7fade88b
Matheus Allan <matheusrpd@ufrn.edu.br>
2018-11-24 22:00:33 -0300

    Estrutura para controle de criação de usuários e atividades criado.

--- InsideThreat/src/br/edu/imd/controller/Controller.java
+++ InsideThreat/src/br/edu/imd/controller/Controller.java
@@ -1,10 +1,23 @@
 package br.edu.imd.controller;
 
-
-
 public class Controller {
 	BildPerfil bild = new BildPerfil();
-	KeepTrees kepp = new KeepTrees();
+	KeepTrees keep = new KeepTrees();
 	AnomalyAnalysis anomaly = new AnomalyAnalysis();
 	
+	public void createUsers(String wayFile) {
+		keep.addUser(bild.buildUser(wayFile));
+	}
+	
+	public void createHttp(String wayFile) {
+		keep.addHttp(bild.buildHttp(wayFile));
+	}
+	
+	public void createLogon(String wayFile) {
+		keep.addLogon(bild.buildLogon(wayFile));
+	}
+	
+	public void createDevices(String wayFile) {
+		keep.addDevices(bild.buildDevices(wayFile));
+	}
 }
--- InsideThreat/src/br/edu/imd/controller/KeepTrees.java
+++ InsideThreat/src/br/edu/imd/controller/KeepTrees.java
@@ -2,16 +2,45 @@ package br.edu.imd.controller;
 
 import java.util.ArrayList;
 
+import br.edu.imd.model.Devices;
+import br.edu.imd.model.Http;
+import br.edu.imd.model.Logon;
 import br.edu.imd.model.NodeUser;
+import br.edu.imd.model.User;
 
 public class KeepTrees {
 	ArrayList<ProfileTree> users;
 	ArrayList<NodeUser> usersAverangeProfile;
 	
+	// Adiciona cada usuário recebido no arrayList em um objeto ProfileTree, tendo assim sua árvore criada 
+	public void addUser(ArrayList<User> users) {
+		
+	}
+	
+	// Adiciona a atividade do tipo Http em seu usuário especifico, utilizando o método addActivity() 
+	// do objeto ProfileTree do user correspondente
+	public void addHttp(ArrayList<Http> buildHttp) {
+	
+	}
+
+	// Adiciona a atividade do tipo Logon em seu usuário especifico, utilizando o método addActivity() 
+	// do objeto ProfileTree do user correspondente
+	public void addLogon(ArrayList<Logon> buildLogon) {
+	
+	}
+
+	// Adiciona a atividade do tipo Devices em seu usuário especifico, utilizando o método addActivity()
+	// do objeto ProfileTree do user correspondente
+	public void addDevices(ArrayList<Devices> buildDevices) {
+		
+	}
+	
+	// Retorna o arrayList que armazena as árvores de cada usuário
 	public ArrayList<ProfileTree> getUsers(){
 		return users;
 	}
-	
+
+	// Retorna o arrayList que armazena os úsuarios de perfil médio de cada papel
 	public ArrayList<NodeUser> getUsersAverangeProfile(){
 		return usersAverangeProfile;
 	}
